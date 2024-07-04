f5534a4258bab736d8ffd62033b9f1b2707c879f
VoidLeech <ian.gerritse@gmail.com>
2024-07-04 02:59:38 +0200

    feat: add quark compat

--- src/main/java/com/teamremastered/endrem/mixin/compat/MatrixEnchantingMenuMixin.java
+++ src/main/java/com/teamremastered/endrem/mixin/compat/MatrixEnchantingMenuMixin.java
@@ -1,24 +1,22 @@
-package com.teamremastered.endrem.mixin;
+package com.teamremastered.endrem.mixin.compat;
 
 import com.teamremastered.endrem.config.ERConfig;
 import com.teamremastered.endrem.registers.ERItems;
 import com.teamremastered.endrem.utils.CrypticEyeDistribution;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.EnchantmentMenu;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.violetmoon.quark.addons.oddities.inventory.MatrixEnchantingMenu;
 
 import java.util.Random;
 
-@SuppressWarnings("unused")
-@Mixin(EnchantmentMenu.class)
-public class PlayerEnchantMixin {
-
-    @Inject(method = "clickMenuButton", at = @At(value = "RETURN", ordinal = 2))
-    private void isEnchanting(final Player player, final int id, final CallbackInfoReturnable<Boolean> info) {
+@Mixin(MatrixEnchantingMenu.class)
+public class MatrixEnchantingMenuMixin {
+    @Inject(method = "finish", at = @At("TAIL"), remap = false)
+    private void endrem$finishEnchantingAtMatrixTable(Player player, ItemStack stack, CallbackInfo ci){
         CrypticEyeDistribution.rollForCrypticEye(player);
     }
 }
